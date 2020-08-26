fb37e07be7e17f4c3296f1bfd7daa5a489ed6cb3
realstonesword <efitterer08@gmail.com>
2020-08-26 14:47:51 -0500

    Random Number Generator now works!

--- template.c
+++ template.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+int randomNumber();
 void usage() {
     printf("skldhjaskldjsayhdilsahduqidhkl\n");
 }
 
 
 int main() {
-    
+
+    printf("%d", randomNumber());
+
+
     return 0;
+}
+
+int randomNumber() {
+
+    int rand, finalValue;
+        srand(time(0));
+        rand = random();
+        finalValue = (rand % 366) + 1;
+    return finalValue;
 }
