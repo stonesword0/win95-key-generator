ea2cd304c046619a0d34abe0af7a96afa88d817f
realstonesword <efitterer08@gmail.com>
2020-08-27 16:01:03 -0500

    Working

--- template.c
+++ template.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-char win95();
+int win95();
 int randomNumber(int upper, int lower);
 void usage() {
     printf("skldhjaskldjsayhdilsahduqidhkl\n");
@@ -9,25 +9,40 @@ void usage() {
 
 
 int main() {
-    int temp1 = 7, temp2 = 13, temp3;
-    temp3 = temp2 % temp1;
-    printf("%d", temp3);
-
+    win95();
 
     return 0;
 }
 
-char win95(){
+int win95(){
+    
     int chunk1 = randomNumber(366, 100);
     int chunk2 = randomNumber(99, 96);
     const char *chunk3 = "OEM";
-    int chunk4[6];
-    int chunk5[5];
-    char final[23];
-
-    printf("%s", chunk3);
-
-
+    int chunk4[5];
+    int chunk5 = randomNumber(99999, 10000);
+    char final;
+    
+    while((chunk4[0] + chunk4[1] + chunk4[2] + chunk4[3] + chunk4[4] + chunk4[5]) % 7 !=  0) {
+        printf("Brute Forcing...\n");
+        printf("DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
+        chunk4[0] = randomNumber(9, 1);
+        sleep(1);
+        chunk4[1] = randomNumber(9, 1);
+        sleep(1);
+        chunk4[2] = randomNumber(9, 1);
+        sleep(1);
+        chunk4[3] = randomNumber(9, 1);
+        sleep(1);
+        chunk4[4] = randomNumber(9, 1);
+        sleep(1);
+        chunk4[5] = randomNumber(7, 1);
+        sleep(1);
+    }
+    
+    printf("FINAL DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
+    printf("%d%d-OEM-0%d%d%d%d%d%d-%d", chunk1, chunk2, chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5], chunk5);
+    return 0;
 }
 
 int randomNumber(int upper, int lower) {
