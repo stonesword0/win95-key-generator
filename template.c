9cc1b65a67b29d9a15f24fe28e6879fd890dd85e
RealStoneSword <58371315+RealStoneSword@users.noreply.github.com>
2020-08-28 14:47:04 -0500

    Finidhed

--- template.c
+++ template.c
@@ -27,28 +27,24 @@ int win95(){
         printf("Brute Forcing...\n");
         printf("DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
         chunk4[0] = randomNumber(9, 1);
-        sleep(1);
         chunk4[1] = randomNumber(9, 1);
-        sleep(1);
         chunk4[2] = randomNumber(9, 1);
-        sleep(1);
         chunk4[3] = randomNumber(9, 1);
-        sleep(1);
         chunk4[4] = randomNumber(9, 1);
-        sleep(1);
         chunk4[5] = randomNumber(7, 1);
-        sleep(1);
     }
     
     printf("FINAL DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
-    printf("%d%d-OEM-0%d%d%d%d%d%d-%d", chunk1, chunk2, chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5], chunk5);
+    printf("%d%d-OEM-0%d%d%d%d%d%d-%d\n", chunk1, chunk2, chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5], chunk5);
     return 0;
 }
 
 int randomNumber(int upper, int lower) {
+    int seed;
+    
 
     int rand, finalValue;
-        srand(time(0));
+        srand(time(NULL));
         rand = random();
         finalValue = (rand % (upper - lower +1) ) + lower;
     return finalValue;
