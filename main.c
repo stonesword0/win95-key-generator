4bb65e11aaac023102816577740dbcacbc1df614
realstonesword <efitterer08@gmail.com>
2020-09-09 20:52:22 -0500

    Added windows 95 cd keys

--- main.c
+++ main.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int win95();
 int randomNumber(int upper, int lower);
 void usage() {
-    printf("skldhjaskldjsayhdilsahduqidhkl\n");
+    printf("ilsahduqidhkl\n");
 }
 
 
 int main() {
-    win95oem();
+    win95cdkey();
 
     return 0;
 }
 
 int win95cdkey() {
+    srand(time(0));
+    int chunk1 = (rand() % (998 - 100 +1)) + 100;
+    if(chunk1 == 333 || chunk1 == 444 || chunk1 == 555 || chunk1 == 666 || chunk1 == 777 || chunk1 == 888 || chunk1 == 999) {
+        chunk1 = 635;
+    }
+    int chunk2[7];
+    for(int i = 0; i <= 5; i++) {
+        chunk2[i] = 6;
+    }
 
-    
+    while((chunk2[0] + chunk2[1] + chunk2[2] + chunk2[3] + chunk2[4] + chunk2[5] + chunk2[6]) % 7 !=  0) {
+        printf("Brute Forcing...\n");
+        printf("DEBUG\nchunk2[0]: %d\nchunk2[1]: %d\nchunk2[2]: %d\nchunk2[3]: %d\nchunk2[4]: %d\nchunk2[5]: %d\nchunk2[6]: %d\n",chunk2[0], chunk2[1], chunk2[2], chunk2[3], chunk2[4], chunk2[5], chunk2[6]);
+        for(int i = 0; i <= 5; i++) {
+            
+            chunk2[i] = (rand() % (9 - 1 +1)) + 1;
+        }
+        chunk2[6] = (rand() % (7 - 1 +1)) + 1;
+    }
 
+    printf("FINAL DEBUG\nchunk2[0]: %d\nchunk2[1]: %d\nchunk2[2]: %d\nchunk2[3]: %d\nchunk2[4]: %d\nchunk2[5]: %d\nchunk2[6]: %d\n",chunk2[0], chunk2[1], chunk2[2], chunk2[3], chunk2[4], chunk2[5], chunk2[6]);
+    printf("\nKey: %d-%d%d%d%d%d%d%d\n", chunk1, chunk2[0], chunk2[1], chunk2[2], chunk2[3], chunk2[4], chunk2[5], chunk2[6]);
 }
 
 int win95oem(){
@@ -28,27 +46,13 @@ int win95oem(){
     int chunk4[6];
     int chunk5 = (rand() % (99999 - 10000 +1)) + 10000;
     char final;
-    
+
+    chunk4[4] = 5;
+    chunk4[5] = 5;
+
     while((chunk4[0] + chunk4[1] + chunk4[2] + chunk4[3] + chunk4[4] + chunk4[5]) % 7 !=  0) {
+        
         chunk4[5] = 0;
-        if (chunk4[0] >= 10){
-            chunk4[0] = 5;
-        }
-        if (chunk4[1] >= 10){
-            chunk4[1] = 5;
-        }
-        if (chunk4[2] >= 10){
-            chunk4[2] = 5;
-        }
-        if (chunk4[3] >= 10){
-            chunk4[3] = 5;
-        }
-        if (chunk4[4] >= 10){
-            chunk4[4] = 5;
-        }
-        if (chunk4[5] >= 10){
-            chunk4[5] = 5;
-        }
         printf("Brute Forcing...\n");
         printf("DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
         chunk4[0] = (rand() % (9 - 1 +1)) + 1;
@@ -60,14 +64,12 @@ int win95oem(){
     }
     
     printf("FINAL DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
-    printf("Key: %d%d-OEM-0%d%d%d%d%d%d-%d\n\nThanks for using my program!\n\nWARNING: Sometimes the program will generat a invalid key. The FINAL DEBUG will say one of the chunks is over 10. That means something went wrong. I am working to fix this soon! Until it is fixed, just start the program again with the same arguments.\n\n", chunk1, chunk2, chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5], chunk5);
+    printf("Key: %d%d-OEM-0%d%d%d%d%d%d-%d\n\nThanks for using my program!\n\n\n", chunk1, chunk2, chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5], chunk5);
     return 0;
 }
 
 int randomNumber(int upper, int lower) {
     int seed;
-    
-
     int randomm, finalValue;
         srand(time(NULL));
         randomm = rand();
