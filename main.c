37e69abb13e1aeacb555748599872601db0ffedb
realstonesword <efitterer08@gmail.com>
2020-09-13 02:41:09 -0500

    Fixed problems and made code efficent

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ void usage() {
 }
 
 
-int main() {
-    win95cdkey();
+int main(int argc, char *argv[]) {
+    win95oem();
 
     return 0;
 }
@@ -35,34 +35,34 @@ int win95cdkey() {
     }
 
     printf("FINAL DEBUG\nchunk2[0]: %d\nchunk2[1]: %d\nchunk2[2]: %d\nchunk2[3]: %d\nchunk2[4]: %d\nchunk2[5]: %d\nchunk2[6]: %d\n",chunk2[0], chunk2[1], chunk2[2], chunk2[3], chunk2[4], chunk2[5], chunk2[6]);
-    printf("\nKey: %d-%d%d%d%d%d%d%d\n", chunk1, chunk2[0], chunk2[1], chunk2[2], chunk2[3], chunk2[4], chunk2[5], chunk2[6]);
+    printf("\nKey: %d-%d%d%d%d%d%d%d\n\nThanks for using my program!\n\n", chunk1, chunk2[0], chunk2[1], chunk2[2], chunk2[3], chunk2[4], chunk2[5], chunk2[6]);
 }
 
 int win95oem(){
     srand(time(NULL));
     int chunk1 = (rand() % (365 - 100 +1)) + 365;
     int chunk2 = (rand() % (99 - 95 +1)) + 95;
-    const char *chunk3 = "OEM";
+    /* there used to be a chunk3 here, was not needed so it was removed */
     int chunk4[6];
+    for(int i = 0; i <= 4; i++) {
+        chunk4[i] = 4;
+    }
+    chunk4[5] = 2;
     int chunk5 = (rand() % (99999 - 10000 +1)) + 10000;
-    char final;
 
-    chunk4[4] = 5;
-    chunk4[5] = 5;
 
+    
     while((chunk4[0] + chunk4[1] + chunk4[2] + chunk4[3] + chunk4[4] + chunk4[5]) % 7 !=  0) {
         
-        chunk4[5] = 0;
         printf("Brute Forcing...\n");
         printf("DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
-        chunk4[0] = (rand() % (9 - 1 +1)) + 1;
-        chunk4[1] = (rand() % (9 - 1 +1)) + 1;
-        chunk4[2] = (rand() % (9 - 1 +1)) + 1;
-        chunk4[3] = (rand() % (9 - 1 +1)) + 1;
-        chunk4[4] = (rand() % (9 - 1 +1)) + 1;
-        chunk4[5] = (rand() % (7 - 1 +1)) + 1;
+        for(int i = 0; i<= 5; i++) {
+            chunk4[i] = (rand() % (9 - 1 +1)) + 1;
+        }
+        /*chunk4[5] = (rand() % (7 - 1 +1)) + 1;*/
+        
     }
-    
+
     printf("FINAL DEBUG\nchunk4[0] = %d\nchunk4[1] = %d\nchunk4[2] = %d\nchunk4[3] = %d\nchunk4[4] = %d\nchunk4[5] = %d\n\n", chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5]);
     printf("Key: %d%d-OEM-0%d%d%d%d%d%d-%d\n\nThanks for using my program!\n\n\n", chunk1, chunk2, chunk4[0], chunk4[1], chunk4[2], chunk4[3], chunk4[4], chunk4[5], chunk5);
     return 0;
